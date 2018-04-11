2cf766fa3f1bda07e54de9e16b1038b0e7dd42b1
danfrenette <dan.r.frenette@gmail.com>
2018-04-11 12:39:01 -0400

    Create main project components

---
+++ src/components/Inventory.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Inventory extends React.Component {
+  render() {
+    return (
+      <div class="inventory">
+        Sample Inventory...
+      </div>
+    );
+  };
+};
+
+export default Inventory;
---
+++ src/components/Order.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class Order extends React.Component {
+  render() {
+    return (
+      <div className="order">
+        Sample Order Text...
+      </div>
+    );
+  };
+};
+export default Order
