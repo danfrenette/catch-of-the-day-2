031b5acbc97188e2903129d6d23c897f22f69c07
danfrenette <dan.r.frenette@gmail.com>
2018-04-11 23:03:54 -0400

    Setup React Router

---
+++ src/components/NotFound.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NotFound = () => (
+  <div className="not-found">
+    <h2>
+      Sorry, the page you're looking for does not exist.
+    </h2>
+  </div>
+);
+
+export default NotFound;
