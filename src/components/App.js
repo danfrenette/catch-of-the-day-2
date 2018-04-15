ef632b7f1670ce635735c50edbb602d8150f8ab1
danfrenette <dan.r.frenette@gmail.com>
2018-04-15 15:14:04 -0400

    Fishes are persisted in inventory through Firebase

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,27 @@
 import React from "react";
-import Header from "./Header"
-import Inventory from "./Inventory"
-import Order from "./Order"
-import Fish from "./Fish"
-import sampleFishes from "../sample-fishes"
+import Header from "./Header";
+import Inventory from "./Inventory";
+import Order from "./Order";
+import Fish from "./Fish";
+import sampleFishes from "../sample-fishes";
+import base from "../base";
 
 class App extends React.Component {
   state = {
     fishes: {},
     order: {}
+  };
+
+  componentDidMount() {
+    const { params } = this.props.match
+    this.ref = base.syncState(`${params.storeId}/fishes`, {
+      context: this,
+      state: "fishes",
+    });
+  };
+
+  componentWillUnmount() {
+    base.removeBinding(this.ref);
   };
 
   addFish = fish => {
