a4057aea8032f38258bd312a1f5def05cb9132f7
danfrenette <dan.r.frenette@gmail.com>
2018-04-15 19:02:02 -0400

    Orders animate into and out of state

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,16 +1,26 @@
 import React from "react";
 import { formatPrice } from "../helpers";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 
 class Order extends React.Component {
   renderOrder = key => {
     const fish = this.props.fishes[key]
     const isAvailable = fish && fish.status
+    const transitionOptions = {
+      classNames: "order",
+      key,
+      timeout: { enter: 500, exit: 500 },
+    }
+
+    if (!fish) return null;
 
     if (!isAvailable) {
       return (
-        <li key={key}>
-          Sorry, {fish ? fish.name : "this fish"} is no longer available.
-        </li>
+        <CSSTransition {...transitionOptions}>
+          <li key={key}>
+            Sorry, {fish ? fish.name : "this fish"} is no longer available.
+          </li>
+        </CSSTransition>
       )
     } else {
       const count = this.props.order[key]
@@ -19,13 +29,22 @@ class Order extends React.Component {
                            </button>
 
       return (
-        <li key={key}>
-          { count } lbs { fish.name }
-          &nbsp;
-          { formatPrice(count * fish.price) }
-          &nbsp;
-          { removeButton }
-        </li>
+        <CSSTransition {...transitionOptions} >
+          <li key={key}>
+            <span>
+              <TransitionGroup component="span" className="count">
+                <CSSTransition classNames="count" key={count} timeout={{ enter: 500, exit: 500 }}>
+                  <span>{ count }</span>
+                </CSSTransition>
+              </TransitionGroup>
+              lbs { fish.name }
+              &nbsp;
+              { formatPrice(count * fish.price) }
+              &nbsp;
+              { removeButton }
+            </span>
+          </li>
+        </CSSTransition>
       );
     };
   };
@@ -51,7 +70,9 @@ class Order extends React.Component {
     return (
       <div className="order-wrap">
         <h2>Order</h2>
-        <ul className="order">{orderIds.map(this.renderOrder)}</ul>
+        <TransitionGroup className="order" component="ul">
+          {orderIds.map(this.renderOrder)}
+        </TransitionGroup>
         <div className="total">
           Total:
           &nbsp;
